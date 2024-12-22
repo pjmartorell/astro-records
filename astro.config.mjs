f0429a2726a5b0ba6721cca7a3bd5abcca67b8a6
Pere Joan Martorell <pj.martorell@gmail.com>
2024-12-22 21:41:22 +0100

    Remove Cloudflare integration and clean up type definitions in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
 
-import cloudflare from '@astrojs/cloudflare'
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'static',
-  adapter: cloudflare(),
 })
